0757580740f0c7665011a6f8a1536ea55ce7d1d1
seunghyunmoon2 <dktlfkrldh@gmail.com>
2020-07-30 10:07:20 +0900

    added some comments

--- modelbuild.py
+++ modelbuild.py
@@ -1,12 +1,27 @@
 # -*- coding: utf-8 -*-
 
-!pip install KoSpacing
+# !pip install KoSpacing
 
-from hangul_utils import Preprocessor
-p = Preprocessor()
-p.normalize("부들부들부들부들 내가 작간데 화가낰ㅋㅋㅋㅋ")
-#"부들부들 내가 작가인데 화가나ㅋㅋㅋ"
+# from hangul_utils import Preprocessor
+# p = Preprocessor()
+# p.normalize("부들부들부들부들 내가 작간데 화가낰ㅋㅋㅋㅋ")
+# #"부들부들 내가 작가인데 화가나ㅋㅋㅋ"
 
-from hangul_utils import normalize
-normalize("부들부들부들부들 내가 작간데 화가낰ㅋㅋㅋㅋ")
-#"부들부들 내가 작가인데 화가나ㅋㅋㅋ"
+# from hangul_utils import normalize
+# normalize("부들부들부들부들 내가 작간데 화가낰ㅋㅋㅋㅋ")
+# #"부들부들 내가 작가인데 화가나ㅋㅋㅋ"
+
+
+
+
+# unicode.py 실행 필요.
+
+'''is_hangul_syllable('갍')
+Out[6]: True'''
+
+cmt = """
+요번에 오랜만에 외국여행을 친구들이랑 왔는데 여기 집주인이 아주 아니에요. 가격도 그렇게 싼 편이 아닌데 서비스가 엉망이고요, 화장실도 더럽고 우리가 뭐라 하니 화만 내네요 ㅠ.ㅠ 여기 주변 음식점도 다 맛 없고 분위기도 별로에요.
+
+여기 리뷰 잘 써주면 할인해준다고는 해서 쓰고있는데 이 리뷰 보시고 다들 잘 거르시길 바랍니다...
+"""
+
